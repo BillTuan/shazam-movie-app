5b99906d994356bf11f51830af2206f3be573c97
Bill <baotuan0.0@gmail.com>
2019-01-06 22:41:25 +0700

    Add editorconfig file

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import {BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Login from '../screens/Login'
+import Home from '../screens/Home'
 
 const Routing = () => (
 	<Router>
 		<div>
+			<Route exact={true} path='/' component={Home}/>
 			<Route path='/login' component={Login}/>
 		</div>
 	</Router>
